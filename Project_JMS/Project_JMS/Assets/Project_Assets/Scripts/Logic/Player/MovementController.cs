6e6587521696094e2ed1a51f334aa6ecd73665b5
JD <dirkxjordy@gmail.com>
2018-08-11 11:08:13 +0200

    Movement

--- Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/MovementController.cs
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/MovementController.cs
@@ -5,31 +5,42 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     [SerializeField] private Transform backGround;
+    [SerializeField] private Vector2 backGroundClamp = new Vector2(-10, 10);
 
     [SerializeField] private float acceleration = 1f;
     private float currentSpeed = 0f;
-	
+
 
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-            Move();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentSpeed += acceleration;
         }
-
+        Move();
         WrapAround();
-
+        Friction();
     }
 
-	void Move()
-	{
-        float newSpeed = currentSpeed + acceleration;
+    void Move()
+    {
+        Vector3 newPosition = Vector3.zero;
+        newPosition.x -= currentSpeed;
 
-		// backGround.Translate()
+
+        backGround.Translate(newPosition);
     }
 
-	void WrapAround()
-	{
+    void WrapAround()
+    {
+        if (backGround.position.x <= backGroundClamp.x)
+        {
+            backGround.position = new Vector3(backGroundClamp.y, backGround.position.y, backGround.position.z);
+        }
+    }
 
-	}
+    void Friction()
+    {
+        
+    }
 }
