fc95567e3c838bff294c615a9cba83a1bbb4247e
Selwyn <foesho@live.be>
2018-08-11 18:32:23 +0200

    Distortion #02
    Hooked with playerspeed

--- Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Systems/Distorter.cs
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Systems/Distorter.cs
@@ -3,11 +3,13 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class Distorter : MonoBehaviour {
 
+	[SerializeField]	private MovementController controller;
 	[SerializeField]	private PostProcessVolume volume;
 						private LensDistortion lensDistortion;
 	[SerializeField]	private int lensCap = -45;
-	[Range(0,1000)]		public float speed;						// TODO:	Get speed value from movement controller
-	[Range(0,2)]		public float distortionMultiplier;
+	[SerializeField]	private float currentDistortionValue;	// Just to check if it updates		
+						public float speed;						// Linked with speed from movement controller
+	[Range(0,100)]		public float distortionMultiplier;
 	
 	void Start () 
 	{
@@ -20,6 +22,7 @@ public class Distorter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		speed = controller.CurrentSpeed;
 		if (volume != null) 
 		{
 			bool foundLensDistortionSettings = volume.profile.TryGetSettings<LensDistortion>(out lensDistortion);		// Try to find Lens Distortion on Post Process Volume
@@ -28,8 +31,9 @@ public class Distorter : MonoBehaviour {
 				Debug.Log("Can't find lens distortion settings");
 			} 
 			else 
-			{
+			{				
 				lensDistortion.intensity.value = Mathf.Clamp(Mathf.RoundToInt(speed * -distortionMultiplier), lensCap, 0);		// Apply manipulation on Lens Distortion
+				currentDistortionValue = lensDistortion.intensity.value;
 			}
 		} 
 		else 
