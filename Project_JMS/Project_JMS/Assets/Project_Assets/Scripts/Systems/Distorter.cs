e62af4e0e3d5d541b248bd7e9f0862f89ab40752
JD <dirkxjordy@gmail.com>
2018-08-11 18:44:13 +0200

    Distorter lerp

--- Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Systems/Distorter.cs
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Systems/Distorter.cs
@@ -1,50 +1,51 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-public class Distorter : MonoBehaviour {
+public class Distorter : MonoBehaviour
+{
 
-	[SerializeField]	private MovementController controller;
-	[SerializeField]	private PostProcessVolume volume;
-						private LensDistortion lensDistortion;
-	[SerializeField]	private int lensCap = -45;
-	[SerializeField]	private float currentDistortionValue;	// Just to check if it updates		
-						public float speed;						// Linked with speed from movement controller
-	[Range(0,100)]		public float distortionMultiplier;
-	
-	void Start () 
-	{
-		if (volume == null) 
-		{
-			PullPostProcessVolume();
-		}
-	}
+    [SerializeField] private MovementController controller;
+    [SerializeField] private PostProcessVolume volume;
+    private LensDistortion lensDistortion;
+    [SerializeField] private int lensCap = -45;
+    public float speed;                     // Linked with speed from movement controller
+    [Range(0, 100)] public float distortionMultiplier;
 
-	// Update is called once per frame
-	void Update () 
-	{
-		speed = controller.CurrentSpeed;
-		if (volume != null) 
-		{
-			bool foundLensDistortionSettings = volume.profile.TryGetSettings<LensDistortion>(out lensDistortion);		// Try to find Lens Distortion on Post Process Volume
-			if (!foundLensDistortionSettings) 
-			{
-				Debug.Log("Can't find lens distortion settings");
-			} 
-			else 
-			{				
-				lensDistortion.intensity.value = Mathf.Clamp(Mathf.RoundToInt(speed * -distortionMultiplier), lensCap, 0);		// Apply manipulation on Lens Distortion
-				currentDistortionValue = lensDistortion.intensity.value;
-			}
-		} 
-		else 
-		{
-			PullPostProcessVolume();
-		}
-		
-	}
+    void Start()
+    {
+        if (volume == null)
+        {
+            PullPostProcessVolume();
+        }
+        controller = FindObjectOfType<MovementController>();
+    }
 
-	void PullPostProcessVolume () 
-	{
-		volume = GetComponent<PostProcessVolume>();
-	}
+    void Update()
+    {
+        speed = controller.CurrentSpeed;
+        if (volume != null)
+        {
+            bool foundLensDistortionSettings = volume.profile.TryGetSettings<LensDistortion>(out lensDistortion);       // Try to find Lens Distortion on Post Process Volume
+            if (!foundLensDistortionSettings)
+            {
+                Debug.Log("Can't find lens distortion settings");
+            }
+            else
+            {
+                float newIntensity = Mathf.RoundToInt(speed * -distortionMultiplier);
+                newIntensity = Mathf.Clamp(newIntensity, lensCap, 0);
+                lensDistortion.intensity.value = Mathf.Lerp(lensDistortion.intensity.value, newIntensity, Time.deltaTime * 10f);        // Apply manipulation on Lens Distortion
+            }
+        }
+        else
+        {
+            PullPostProcessVolume();
+        }
+
+    }
+
+    void PullPostProcessVolume()
+    {
+        volume = GetComponent<PostProcessVolume>();
+    }
 }
