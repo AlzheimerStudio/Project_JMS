adef6fc55d347957c2255c199b75196ecb36a717
Mark <freak_mania@hotmail.com>
2018-08-12 19:48:20 +0200

    New audio big bang 02

---
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/UI/ButotnPress_sprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButotnPress_sprite : MonoBehaviour
+{
+
+
+    public Sprite unpressed;
+    public Sprite pressed;
+
+    public SpriteRenderer sr;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
