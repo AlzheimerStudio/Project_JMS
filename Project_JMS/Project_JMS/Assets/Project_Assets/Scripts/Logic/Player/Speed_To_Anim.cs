b90d8d7de2ca95616f66838db27cd276bf32b366
Mark <freak_mania@hotmail.com>
2018-08-12 21:41:27 +0200

    Ambient particle update

--- Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/Speed_To_Anim.cs
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/Speed_To_Anim.cs
@@ -10,28 +10,56 @@ public class Speed_To_Anim : MonoBehaviour
     [SerializeField] private Animator anim;
     private MovementController movementController;
     [SerializeField] private ParticleSystem playerTrail;
+    [SerializeField] private ParticleSystem ambientParticles;
+    private ParticleSystem.VelocityOverLifetimeModule velocityModule;//settings container voor velocity over lifetime module.
+    private ParticleSystem.TrailModule trailModule;//settings container voor trail module.
 
     void Start()
     {
         anim = GetComponent<Animator>();
         movementController = GameManager.instance.movementController;
         playerTrail = GetComponentInChildren<ParticleSystem>();
+
+        velocityModule = ambientParticles.velocityOverLifetime;
+        trailModule = ambientParticles.trails;
+
     }
 
     void Update()
     {
-        anim.SetFloat("_Speed", movementController.CurrentSpeed * 10f);
 
         // Controls particle trail
         float speed = movementController.CurrentSpeed;
+        anim.SetFloat("_Speed", speed * 10f);
 
-        if (speed > 0 && playerTrail != null)
+
+        if (speed > 0 && playerTrail != null && ambientParticles != null)
         {
 
             var main = playerTrail.main;
             main.startSpeed = speed.MapRangeClamped(0f, 30f, 0.1f, 50);
             var shape = playerTrail.shape;
             shape.radius = Mathf.Clamp(speed.MapRangeClamped(0f, 20f, .8f, .1f), 0f, .8f);
+
+            if (speed > 1)
+            {
+                velocityModule.speedModifier = speed;
+                trailModule.ratio = 1;
+            }
+            else
+            {
+                velocityModule.speedModifier = 1;
+                trailModule.ratio = 0.2f;
+
+            }
+
+
+        }
+        else
+        {
+            velocityModule.speedModifier = 1;
+            trailModule.ratio = 0.2f;
+
         }
     }
 
