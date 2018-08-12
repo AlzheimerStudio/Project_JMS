6806851a8312648ac6f0974b31dba0601b540d50
Mark <freak_mania@hotmail.com>
2018-08-12 14:05:46 +0200

    Final animation
    animatie bij player geintergeerd
    Art_Atlas geupdate met nieuwe sprites.
    Nieuwe scene voor Art en animation te teste.

---
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/Speed_To_Anim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+public class Speed_To_Anim : MonoBehaviour
+{
+
+    public Animator anim;
+    public MovementController movementController;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (anim == null)
+        {
+
+            anim = GetComponent<Animator>();
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        anim.SetFloat("_Speed", movementController.CurrentSpeed * 10f);
+    }
+}
