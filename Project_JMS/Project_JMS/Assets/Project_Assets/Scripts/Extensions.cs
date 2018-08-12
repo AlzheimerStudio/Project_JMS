10b6e78a26c081e0316bddee8706f17c988fe772
JD <dirkxjordy@gmail.com>
2018-08-12 17:48:49 +0200

    particles n stuff

---
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Extensions.cs
@@ -0,0 +1,9 @@
+﻿
+public static class Extensions
+{
+    public static float MapRangeClamped(this float value, float inA, float outA, float inB, float outB)
+    {
+        return (value - inA) / (outA - inA) * (outB - inB) + inB;
+    }
+
+}
--- Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/Speed_To_Anim.cs
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/Logic/Player/Speed_To_Anim.cs
@@ -7,26 +7,29 @@ using UnityEngine.Animations;
 public class Speed_To_Anim : MonoBehaviour
 {
 
-    public Animator anim;
-    public MovementController movementController;
+    [SerializeField] private Animator anim;
+    private MovementController movementController;
+    [SerializeField] private ParticleSystem playerTrail;
 
-
-    // Use this for initialization
     void Start()
     {
-        if (anim == null)
-        {
-
-            anim = GetComponent<Animator>();
-
-        }
-
+        anim = GetComponent<Animator>();
+        movementController = GameManager.instance.movementController;
+        playerTrail = GetComponentInChildren<ParticleSystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         anim.SetFloat("_Speed", movementController.CurrentSpeed * 10f);
+
+        // Controls particle trail
+
+        float speed = movementController.CurrentSpeed;
+        var main = playerTrail.main;
+        main.startSpeed = speed.MapRangeClamped(0.1f, 30f, 0.1f, 5);
+        var shape = playerTrail.shape;
+        shape.radius = speed.MapRangeClamped(0.1f, 3f, .8f, .1f);
+
+        Debug.Log(1f.MapRangeClamped(1, 3, 0, 10));
     }
 }
