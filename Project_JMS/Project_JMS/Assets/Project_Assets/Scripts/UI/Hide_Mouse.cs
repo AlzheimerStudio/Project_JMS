b6550e5f16ac2dafd557a8132f71d7e588c356aa
Mark <freak_mania@hotmail.com>
2018-08-13 20:03:24 +0200

    fixed pause UI and bugfix mouse visibility

--- Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/UI/Hide_Mouse.cs
+++ Project_JMS/Project_JMS/Assets/Project_Assets/Scripts/UI/Hide_Mouse.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class Hide_Mouse : MonoBehaviour
 {
 
+    private MovementController mC;
+    public bool checkSPD;
+
     // Use this for initialization
     void Start()
     {
         Cursor.visible = false;
+
+        mC = GameManager.instance.movementController; ;
     }
 
     void Update()
     {
-        if (Application.isFocused && !PauseMenu.GamePaused)
+        if (checkSPD)
+        {
+            if (Application.isFocused && !PauseMenu.GamePaused && mC.CurrentSpeed > 0.01)
+            {
+                Cursor.visible = false;
+            }
+        }
+        else if (Application.isFocused)
         {
             Cursor.visible = false;
         }
     }
-
 }
+  
+    
+
+
